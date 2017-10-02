da91b8db5623b03d7d4c76555fb61788a33fb7bf
Diogo Azevedo <diogoazevedos@gmail.com>
2017-10-01 23:03:18 -0300

    feat: Add packer lzw algorithm

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const LZW = require('node-lzw');
 const { PPM } = require('compressjs');
 
-exports.encode = PPM.compressFile;
+/**
+ * Encode the given buffer.
+ *
+ * @param  {Buffer}  buffer
+ * @return {Buffer}
+ */
+exports.encode = (buffer) => {
+  const content = buffer.toString('utf8');
+  const encoded = Buffer.from(LZW.encode(content), 'utf8');
 
-exports.decode = PPM.decompressFile;
+  return PPM.compressFile(encoded);
+};
+
+/**
+ * Decode the given buffer.
+ *
+ * @param  {Buffer}  buffer
+ * @return {Buffer}
+ */
+exports.decode = (buffer) => {
+  const decoded = PPM.decompressFile(buffer);
+  const content = Buffer.from(decoded).toString('utf8');
+
+  return Buffer.from(LZW.decode(content), 'utf8');
+};
